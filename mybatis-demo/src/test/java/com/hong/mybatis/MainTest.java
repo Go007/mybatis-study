3a79c0296523179008f4bbcef4dabbf9830b7e65
hong <1536054473@qq.com>
2020-08-22 21:19:37 +0800

    MyBatis的插件机制

--- mybatis-demo/src/test/java/com/hong/mybatis/MainTest.java
+++ mybatis-demo/src/test/java/com/hong/mybatis/MainTest.java
@@ -47,6 +47,38 @@ public class MainTest {
      * 一个sqlSession就是代表和数据库的一次会话，用完关闭
      * 3）、使用sql的唯一标志来告诉MyBatis执行哪个sql。sql都是保存在sql映射文件中的。
      *
+     * /**
+     * 	 * 1、获取sqlSessionFactory对象:
+     * 	 * 		解析文件的每一个信息保存在Configuration中，返回包含Configuration的DefaultSqlSession；
+     * 	 * 		注意：【MappedStatement】：代表一个增删改查的详细信息
+     * 	 *
+     * 	 * 2、获取sqlSession对象
+     * 	 * 		返回一个DefaultSQlSession对象，包含Executor和Configuration;
+     * 	 * 		这一步会创建Executor对象；
+     * 	 *
+     * 	 * 3、获取接口的代理对象（MapperProxy）
+     * 	 * 		getMapper，使用MapperProxyFactory创建一个MapperProxy的代理对象
+     * 	 * 		代理对象里面包含了，DefaultSqlSession（Executor）
+     * 	 * 4、执行增删改查方法
+     * 	 *
+     * 	 * 总结：
+     * 	 * 	1、根据配置文件（全局，sql映射）初始化出Configuration对象
+     * 	 * 	2、创建一个DefaultSqlSession对象，
+     * 	 * 		他里面包含Configuration以及
+     * 	 * 		Executor（根据全局配置文件中的defaultExecutorType创建出对应的Executor）
+     * 	 *  3、DefaultSqlSession.getMapper（）：拿到Mapper接口对应的MapperProxy；
+     * 	 *  4、MapperProxy里面有（DefaultSqlSession）；
+     * 	 *  5、执行增删改查方法：
+     * 	 *  		1）、调用DefaultSqlSession的增删改查（Executor）；
+     * 	 *  		2）、会创建一个StatementHandler对象。
+     * 	 *  			（同时也会创建出ParameterHandler和ResultSetHandler）
+     * 	 *  		3）、调用StatementHandler预编译参数以及设置参数值;
+     * 	 *  			使用ParameterHandler来给sql设置参数
+     * 	 *  		4）、调用StatementHandler的增删改查方法；
+     * 	 *  		5）、ResultSetHandler封装结果
+     * 	 *  注意：
+     * 	 *  	四大对象每个创建的时候都有一个interceptorChain.pluginAll(parameterHandler);
+     * 	 *
      * @throws IOException
      */
     @Test
---
+++ mybatis-plugin/src/main/java/com/hong/plugin/dao/EmployeeMapper.java
@@ -0,0 +1,9 @@
+package com.hong.plugin.dao;
+
+import com.hong.plugin.bean.Employee;
+
+public interface EmployeeMapper {
+	
+	public Employee getEmpById(Integer id);
+
+}
